3fcc4e0dcca81e5edaf4ec0ba7b8712cb1fdf11c
MRPERFECT0603 <vivekshaurya62@gmail.com>
2025-03-15 02:26:23 +0530

    changed base url

--- axios.js
+++ axios.js
@@ -2,6 +2,7 @@
 import axios from "axios";
 
 export const contextRequest = axios.create({
-  baseURL: "http://localhost:8101/api",
+  // baseURL: "http://localhost:8101/api",
+  baseURL: "https://smartinbox-backend.onrender.com/api",
   withCredentials: true,
 });
